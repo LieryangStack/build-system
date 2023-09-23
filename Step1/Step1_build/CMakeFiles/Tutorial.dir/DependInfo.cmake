c8c16a9c66eed997d69ff848fb01385d6f4fc7b3
LieryangStack <sxlisibo@gmail.com>
2023-09-23 17:59:58 +0800

    update

--- Step1/Step1_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
+++ Step1/Step1_build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -1,16 +1,14 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/lieryang/Desktop/build-system/Step1/tutorial.cxx" "/home/lieryang/Desktop/build-system/Step1/Step1_build/CMakeFiles/Tutorial.dir/tutorial.cxx.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "."
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lieryang/Desktop/build-system/Step1/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
   )
 
 # Targets to which this target links.
